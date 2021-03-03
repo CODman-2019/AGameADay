3416781e9f446fba35761706479384d42db99e59
CODman-2019 <55792105+CODman-2019@users.noreply.github.com>
2021-03-02 20:33:08 -0400

    Game 1 made

---
+++ A game A Day/Assets/Scripts/Player Controls/Player_4WayController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_4WayController : MonoBehaviour
+{
+
+    public float speed;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0, speed * Time.deltaTime, 0);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, -speed * Time.deltaTime, 0);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(speed * Time.deltaTime, 0, 0);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-speed * Time.deltaTime, 0, 0);
+        }
+    }
+}
---
+++ A game A Day/Assets/Scripts/Spawning/RainObjectSpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RainObjectSpawner : MonoBehaviour
+{
+    public GameObject obj;
+    public float minSpawn, maxSpawn, timer;
+
+    float timeSet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeSet = Time.time + timer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time > timeSet)
+        {
+            Instantiate(obj);
+            Vector3 pos = new Vector3(Random.Range(minSpawn, maxSpawn), transform.position.y, 0f);
+            obj.transform.position = pos;
+            timeSet = Time.time + timer;
+        }
+
+    }
+}
