c1e67d56841068b7f1b60b57c6468355c34376da
CODman-2019 <55792105+CODman-2019@users.noreply.github.com>
2021-03-05 20:56:46 -0400

    Game 4 created

---
+++ A game A Day/Assets/Scripts/Player Controls/2D controls/Player_2DVertical.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_2DVertical : MonoBehaviour
+{
+    public KeyCode left, left_alt, right, right_alt;
+    public float speed;
+
+    int direction;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(left) || Input.GetKey(left_alt))
+        {
+            direction = -1;
+        }
+        else if (Input.GetKey(right) || Input.GetKey(right_alt))
+        {
+            direction = 1;
+        }
+        else
+        {
+            direction = 0;
+        }
+
+
+        transform.Translate(direction * speed * Time.deltaTime, 0, 0);
+    }
+}
