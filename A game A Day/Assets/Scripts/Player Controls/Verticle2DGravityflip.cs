7c36d201e11f64beb5a8f183a3f4a01aad865705
CODman-2019 <55792105+CODman-2019@users.noreply.github.com>
2021-03-05 20:14:56 -0400

    Game made

---
+++ A game A Day/Assets/Scripts/Player Controls/Verticle2DGravityflip.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Verticle2DGravityflip : MonoBehaviour
+{
+    public float lift_Force;
+
+    float gravity;
+    Rigidbody rb;
+    bool flipGravity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        flipGravity = false;
+        rb = this.GetComponent<Rigidbody>();
+        gravity = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) Flip();
+
+        switch (flipGravity)
+        {
+            case true:
+                gravity = lift_Force;
+                break;
+            case false:
+                gravity = -lift_Force;
+                break;
+        }
+
+        rb.AddForce(new Vector3(0, gravity, 0), ForceMode.Impulse);
+
+    }
+
+    void Flip()
+    {
+        if (flipGravity) flipGravity = false;
+        else { flipGravity = true; }
+    }
+}
