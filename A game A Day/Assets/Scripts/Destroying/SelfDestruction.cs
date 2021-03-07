456a18f8b249750321032485504cf6aed98f944d
CODman-2019 <55792105+CODman-2019@users.noreply.github.com>
2021-03-07 12:03:21 -0400

    Game 6 made

---
+++ A game A Day/Assets/Scripts/Destroying/SelfDestruction.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestruction : MonoBehaviour
+{
+    [Header("if randTimer not used - timeMin wll be used")]
+    public bool randTimer;
+    public float timeMin, timeMax;
+
+    float timer;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        float time = timeMin;
+        if(randTimer) time = Random.Range(timeMin, timeMax);
+
+        timer = Time.time + time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > timer) SelfDestroy();
+    }
+
+    void SelfDestroy()
+    {
+        Destroy(this.gameObject);
+    }
+}
--- A game A Day/Assets/Scripts/Triggers/Pickup.cs
+++ A game A Day/Assets/Scripts/Triggers/Pickup.cs
@@ -5,11 +5,6 @@ using UnityEngine;
 public class Pickup : MonoBehaviour
 {
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     private void OnTriggerEnter(Collider other)
     {
