07033e3c2ef3916ab42787da9f38a7c7a6ae344f
CODman-2019 <55792105+CODman-2019@users.noreply.github.com>
2021-03-17 00:41:41 -0300

    game 15 made

---
+++ A game A Day/Assets/Scripts/Level/RotationReset.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationReset : MonoBehaviour
+{
+    public KeyCode button;
+    public float resetPercent;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(button))
+        {
+            gameObject.transform.Rotate(-gameObject.transform.rotation.x * resetPercent, -gameObject.transform.rotation.y * resetPercent, -gameObject.transform.rotation.z * resetPercent);
+
+        }
+    }
+}
---
+++ A game A Day/Assets/Scripts/Spawning/CheckPoints/CheckPointSpawning.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPointSpawning : MonoBehaviour
+{
+    GameObject checkpoint;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Checkpoint"))
+        {
+            checkpoint = other.gameObject;
+        }
+
+        if (other.CompareTag("Killzone"))
+        {
+            this.gameObject.transform.position = checkpoint.transform.position;
+        }
+    }
+}
