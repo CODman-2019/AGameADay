8252b454e47b7843238654a5a1154d6d1fc6ca4c
CODman-2019 <55792105+CODman-2019@users.noreply.github.com>
2021-03-21 23:51:24 -0300

    Game 20 made

---
+++ A game A Day/Assets/Scripts/Spawning/object_Adjustments/SpeedTriggerFlipped.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedTriggerFlipped : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            gameObject.GetComponent<Movement>().speed *= -1;
+            gameObject.transform.Translate(0f, 0f, 1f);
+        }
+    }
+}
