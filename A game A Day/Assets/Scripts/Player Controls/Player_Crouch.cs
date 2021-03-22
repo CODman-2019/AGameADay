b816ff037de87208e482e5b50465707d616efbe8
CODman-2019 <55792105+CODman-2019@users.noreply.github.com>
2021-03-22 18:51:09 -0300

    Game 21 made

---
+++ A game A Day/Assets/Scripts/Player Controls/Player_Crouch.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Crouch : MonoBehaviour
+{
+    public KeyCode input;
+    public float crouchScale, standingScale;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(input))
+        {
+            gameObject.transform.localScale = new Vector3(gameObject.transform.localScale.x, crouchScale, gameObject.transform.localScale.z);
+        }
+        else
+        {
+            gameObject.transform.localScale = new Vector3(gameObject.transform.localScale.x, standingScale, gameObject.transform.localScale.z);
+        }
+    }
+}
---
+++ A game A Day/Assets/Scripts/Triggers/ResetPositionTrigger.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetPositionTrigger : MonoBehaviour
+{
+    public GameObject startPoint;
+    GameObject checkPoint;
+    bool resetPos;
+    // Update is called once per frame
+    void Update()
+    {
+        if (resetPos)
+        {
+            if (checkPoint)
+            {
+                gameObject.transform.position = checkPoint.transform.position;
+            }
+            else
+            {
+                gameObject.transform.position = startPoint.transform.position;
+            }
+            resetPos = false;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Checkpoint"))
+        {
+            checkPoint = other.gameObject;
+            //resetPos = false;
+        }
+
+        if (other.CompareTag("Killzone"))
+        {
+            resetPos = true;
+        }
+
+        if (other.CompareTag("Endpoint"))
+        {
+            checkPoint = null;
+            resetPos = true;
+        }
+    }
+}
