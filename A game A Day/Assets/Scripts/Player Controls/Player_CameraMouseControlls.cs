da3abe0022f4bd44c0930e02cbcc520c79d7b5aa
CODman-2019 <55792105+CODman-2019@users.noreply.github.com>
2021-03-25 19:21:50 -0300

    Game 24 made

---
+++ A game A Day/Assets/Scripts/Player Controls/Player_CameraMouseControlls.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_CameraMouseControlls : MonoBehaviour
+{
+    public Transform playerBody;
+    public float sensitivity;
+    float mouseX, mouseY, rotationX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+
+        rotationX -= mouseY;
+        rotationX = Mathf.Clamp(rotationX, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(rotationX, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ A game A Day/Assets/Scripts/Player Controls/Player_Jump2.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Jump2 : MonoBehaviour
+{
+    public KeyCode jumpButton;
+    public float groundDistance, jumpForce;
+    public Transform ground_Check;
+    public LayerMask ground;
+
+    bool onGround;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        onGround = Physics.CheckSphere(ground_Check.position, groundDistance, ground);
+
+        if (onGround)
+        {
+            if (Input.GetKeyDown(jumpButton))
+            {
+                this.GetComponent<Rigidbody>().AddForce(new Vector3(0f, jumpForce, 0f), ForceMode.Impulse);
+            }
+        }
+    }
+}
