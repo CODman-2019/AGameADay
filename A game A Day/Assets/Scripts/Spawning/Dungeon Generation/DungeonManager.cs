5755376d0a1af11ec2ae466123e4638f51d17b1b
CODman-2019 <55792105+CODman-2019@users.noreply.github.com>
2021-03-29 00:27:27 -0300

    Game 27 made (FAIL)

---
+++ A game A Day/Assets/Scripts/Spawning/Dungeon Generation/DungeonManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DungeonManager : MonoBehaviour
+{
+    public static DungeonManager floor;
+    public bool IsSingleton;
+    public int roomLimit;
+    int roomsUsed = 0;
+    bool spawnRooms;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        SingletonizeDungeonManager();
+        spawnRooms = true;
+    }
+
+    private void SingletonizeDungeonManager()
+    {
+        if (floor == null)
+        {
+            floor = this;
+        }
+        else Destroy(gameObject);
+    }
+
+    public void RoomIncrease() {
+        if (roomsUsed >= roomLimit) spawnRooms = false;
+        else
+        {
+            spawnRooms = true;
+            roomsUsed++;
+        }
+    }
+    public void RoomDecrease()
+    {
+        roomsUsed--;
+    }
+
+    public bool CheckR_Limit() => spawnRooms;
+
+    // Update is called once per frame
+    //void Update()
+    //{
+
+    //}
+}
---
+++ A game A Day/Assets/Scripts/Spawning/Dungeon Generation/RoomSpawn.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class RoomSpawn : MonoBehaviour
+{
+    public GameObject[] roomSelection;
+    bool roomPlaced;
+    //float timer = 100f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        roomPlaced = false;
+        PlaceRoom();
+    }
+
+
+    void PlaceRoom()
+    {
+        if (DungeonManager.floor.CheckR_Limit())
+        {
+            StartCoroutine(Wait());
+            int roomNum = Random.Range(0, roomSelection.Length);
+            Instantiate(roomSelection[roomNum], gameObject.transform);
+            roomPlaced = true;
+            DungeonManager.floor.RoomIncrease();
+        }
+    }
+
+    bool IsPlaced() => roomPlaced;
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(100f);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Room"))
+        {
+            Debug.Log(other.gameObject);
+            DungeonManager.floor.RoomDecrease();
+            Destroy(this.gameObject);
+        }
+
+    }
+
+}
