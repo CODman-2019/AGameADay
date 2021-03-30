9f8d8ff2de1d414dace922bab64d8fa8e5995852
CODman-2019 <55792105+CODman-2019@users.noreply.github.com>
2021-03-29 22:08:25 -0300

    Game 28 made

---
+++ A game A Day/Assets/Scripts/Level/AlphaChange.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlphaChange : MonoBehaviour
+{
+    public GameObject target;
+    [Range(0f, 1f)]
+    public float alphaValue;
+    public float timer;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            LeanTween.alpha(target, alphaValue, timer);
+        }
+    }
+
+    //IEnumerator Timewait()
+    //{
+    //    yield return new WaitForSeconds(1);
+    //    Color alphaChange = target.GetComponent<Renderer>().material.color;
+    //    alphaChange.a /= 2;
+    //    target.GetComponent<Renderer>().material.color = alphaChange;
+    //}
+}
